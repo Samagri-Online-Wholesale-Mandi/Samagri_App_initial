65916160a7e8e3a4895ea8d74c089ad223837078
Riyo <pradeepbhatm25@gmail.com>
2021-08-12 19:39:44 +0530

    homePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:home_page/pages/Commodity_page/tempHome.dart';
 // import 'package:home_page/Models/Product.dart';
 
-import 'BottomNavigation.dart';
-import 'pages/Commodity_page/appBar.dart';
-import 'pages/Commodity_page/body.dart';
-import 'sideDrawer.dart';
+// import 'BottomNavigation.dart';
+// import 'pages/Commodity_page/appBar.dart';
+// import 'pages/Commodity_page/body.dart';
+// import 'sideDrawer.dart';
+import 'pages/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.grey[700],
   ));
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CommodityHomePage(),
+      home: LoginScreen(),
     )
   );
 }
 
-class CommodityHomePage extends StatelessWidget{
+class InitializerWidget extends StatefulWidget {
+  @override
+  _InitializerWidgetState createState() => _InitializerWidgetState();
+}
+
+class _InitializerWidgetState extends State<InitializerWidget> {
+
+  late FirebaseAuth _auth;
+  late User _user;
+  bool isLoading = true;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _auth = FirebaseAuth.instance;
+    _user = _auth.currentUser!;
+    isLoading = false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value:  SystemUiOverlayStyle.light,
-      child: Scaffold(
-        backgroundColor: Colors.grey[900],
-        appBar: appbar(),
-        drawer: sideDrawer(),
-        body: Body(),
-        bottomNavigationBar: BottomNavigation(),
-        )
-    );
+    return isLoading ? Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    ) : _user == null ? LoginScreen() : TempHome();
   }
 }
-
+// class CommodityHomePage extends StatelessWidget{
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AnnotatedRegion<SystemUiOverlayStyle>(
+//       value:  SystemUiOverlayStyle.light,
+//       child: Scaffold(
+//         backgroundColor: Colors.grey[900],
+//         appBar: appbar(),
+//         drawer: sideDrawer(),
+//         body: Body(),
+//         bottomNavigationBar: BottomNavigation(),
+//         )
+//     );
+//   }
+// }
+//
 
 
 
